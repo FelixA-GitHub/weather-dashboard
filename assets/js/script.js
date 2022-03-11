f29fa9afe23e52a12eb7ed836e62374e4e390432
Your Name <felix.acevedo.tech@gmail.com>
2022-03-11 16:45:33 -0500

    working on functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,44 @@
-var url = "http://api.openweathermap.org/data/2.5/weather?q=Atlanta,ga,usa&units=imperial&APPID=33d05024949e3f3d2fc78856ad6d0554"; //enter a valid url 
-//use fetch to get some data from the api!
-fetch(url)
-    .then(function (response) {
-        console.log(response);
-        return response.json();
+//variable to display the date with format preference
+let currentWDate = moment().format(' (MM/DD/YYYY)');
+let currentWCity = "City";
+let currentWIcon = " 🌤";
+
+$("#current-weather-city").html(currentWCity);
+$(".current-weather-date").html(currentWCity + currentWDate + currentWIcon);
+
+
+var getWeather = function(coords) {
+    //make the api call to get the weather based on a set of coordinates
+    var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coords.lat + "&lon=" + coords.lng + "&units=imperial&exclude=minutely,hourly&appid=33d05024949e3f3d2fc78856ad6d0554";
+    fetch(weatherApiUrl).then(function(response){
+        if (response.ok) {
+            response.json().then(function(data){
+                displayCurrWeather(data);  // display the current weather and forecast
+            })
+        } else {
+            console.log("Couldn't get the weather data from the openweathermap API: ", response.text);
+        }
     })
-    .then(function (data) {
-        //do something with the json data
-        console.log(data);
-    });
+}
+
+var displayIcon = function() {
+
+}
+
+var displayCurrWeather = function() {
+
+}
+
+var displayForecast = function() {
+
+}
+
+var searchButton = function(){
+    
+}
+
+var displaySearchHistory = function() {
+
+}
+
+//event handlers
